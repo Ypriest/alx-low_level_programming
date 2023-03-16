3ec763ee1d31c3f46c07530be38c0dd75961b495
Ypriest <anuforoc3@gmail.com>
2023-03-16 12:07:16 -0700

    include

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 /**
  * main -print a line of code using puts
- *
- * Return: 0
+ * return : always 0 (success)
  */
 
 int main(void)
 {
-	put("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
