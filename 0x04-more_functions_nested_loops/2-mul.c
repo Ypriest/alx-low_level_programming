7e15056aa0e16496fa43185424d5b9ab09c27b67
Ypriest <anuforoc3@gmail.com>
2023-03-23 15:27:27 -0700

    gcc: error: _putchar.c: No such file or directory

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - multiply 2 num
+ * @c: first num
+ * @c: second num
+ *
+ * Return: addition of the multiplication
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
